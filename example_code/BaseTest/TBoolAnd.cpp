94edba15d5ad87bbe53497332924bf0c0e7b97e1
2023024333 <2023024333@m.scnu.edu.cn>
2024-05-21 19:36:01 +0800

    ast

--- example_code/BaseTest/TBoolAnd.cpp
+++ example_code/BaseTest/TBoolAnd.cpp
@@ -1,21 +1,14 @@
 // booland_benchmark.cpp
-#include <chrono>
 #include <iostream>
 
 int main() {
-    const long long numIterations = 1000000;
+    const long long numIterations = 1;
     bool a = true, b = false;
     volatile bool result;
-
-    auto start = std::chrono::steady_clock::now();
 
     for (long long i = 0; i < numIterations; ++i) {
         result = a && b;
     }
-
-    auto end = std::chrono::steady_clock::now();
-    std::chrono::duration<double, std::micro> duration = end - start;
-    std::cout << "Time for AND operations: " << duration.count() << " microseconds" << std::endl;
 
     return 0;
 }
